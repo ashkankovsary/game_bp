2f5aafc913eca06b66c6218845e6f87b89901c7f
ashkankovsary <ashkankovsaryyy@gmail.com>
2025-12-26 19:39:53 +0330

    function of drawing player and movement

--- include/player.h
+++ include/player.h
@@ -1,4 +1,16 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include "raylib.h"
 
+typedef struct
+{
+    Vector2 pos;
+    Vector2 dir;
+} Player;
+
+void draw_player(Vector2 player_pos, Vector2 player_dir);
+Player movement_forward(Player player1, float dt);
+Player movement_backward(Player player1, float dt);
+Player movement_right(Player player1, float dt);
+Player movement_left(Player player1, float dt);
 #endif
