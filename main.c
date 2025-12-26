8216af4f86159780b73de490e707e3edbe2368ff
ashkankovsary <ashkankovsaryyy@gmail.com>
2025-12-26 19:36:34 +0330

    movement of player to right and left and backward and forward

--- main.c
+++ main.c
@@ -11,12 +11,27 @@
 
 int main()
 {
+    SetConfigFlags(FLAG_WINDOW_HIGHDPI);
     InitWindow(700, 700, "Raycaster");
+    Player player1;
+    player1.pos = (Vector2){100.0f, 100.0f};
+    player1.dir = (Vector2){1.0f, 0.0f};
+    SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        float dt = GetFrameTime();
+        if (IsKeyDown(KEY_W))
+            player1 = movement_forward(player1, dt);
+        if (IsKeyDown(KEY_S))
+            player1 = movement_backward(player1, dt);
+        if (IsKeyDown(KEY_D))
+            player1 = movement_right(player1, dt);
+        if (IsKeyDown(KEY_A))
+            player1 = movement_left(player1, dt);
         BeginDrawing();
         ClearBackground(bg_1);
         DrawMap();
+        draw_player(player1.pos, player1.dir);
         EndDrawing();
     }
     CloseWindow();
--- player.c
+++ player.c
@@ -3,3 +3,33 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "raylib.h"
+#include "player.h"
+
+#define player_size 7.0f
+#define MOVE_SPEED 90.0f
+
+void draw_player(Vector2 player_pos, Vector2 player_dir)
+{
+    DrawCircleV(player_pos, player_size, RED);
+    DrawLineV(player_pos, (Vector2){player_pos.x + player_dir.x * 20, player_pos.y + player_dir.y * 20}, BLUE);
+}
+Player movement_forward(Player player1, float dt)
+{
+    player1.pos.x += MOVE_SPEED * dt;
+    return player1;
+}
+Player movement_backward(Player player1, float dt)
+{
+    player1.pos.x -= MOVE_SPEED * dt;
+    return player1;
+}
+Player movement_right(Player player1, float dt)
+{
+    player1.pos.y += MOVE_SPEED * dt * 0.7;
+    return player1;
+}
+Player movement_left(Player player1, float dt)
+{
+    player1.pos.y -= MOVE_SPEED * dt * 0.7;
+    return player1;
+}
