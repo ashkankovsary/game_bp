4e63ba87c229fef8be02ecfdf101bc49ae488c2d
ashkankovsary <ashkankovsaryyy@gmail.com>
2025-12-26 19:55:16 +0330

    added function of movement with dir

--- include/player.h
+++ include/player.h
@@ -6,6 +6,7 @@ typedef struct
 {
     Vector2 pos;
     Vector2 dir;
+    Vector2 plane;
 } Player;
 
 void draw_player(Vector2 player_pos, Vector2 player_dir);
@@ -13,4 +14,7 @@ Player movement_forward(Player player1, float dt);
 Player movement_backward(Player player1, float dt);
 Player movement_right(Player player1, float dt);
 Player movement_left(Player player1, float dt);
+Player movement_counterclock_wise(Player player1, float dt);
+Player movement_clock_wise(Player player1, float dt);
+
 #endif
